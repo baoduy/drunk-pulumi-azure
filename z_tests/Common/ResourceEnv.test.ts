434a44963a23c92a285e580785d13c4def068abd
Steven Hoang <hoangbaoduy@gmail.com>
2022-12-30 08:09:14 +0800

    Fixed unit tests

--- z_tests/Common/ResourceEnv.test.ts
+++ z_tests/Common/ResourceEnv.test.ts
@@ -1,56 +1,56 @@
-import * as common from '../../Common/ResourceEnv';
-import { expect } from 'chai';
+import * as common from "../../Common/ResourceEnv";
+import { expect } from "chai";
 import {
   getResourceGroupName,
   getSecretName,
   getStorageName,
-} from '../../Common/Naming';
+} from "../../Common/Naming";
 
-describe('ResourceEnv tests', () => {
-  it('Get Resource Group Name', () => {
-    const name = getResourceGroupName('Resource');
-    expect(name).to.be.equal('stack-resource-grp-hbd');
+describe("ResourceEnv tests", () => {
+  it("Get Resource Group Name", () => {
+    const name = getResourceGroupName("Resource");
+    expect(name).to.be.equal("stack-resource-grp-hbd");
   });
 
-  it('Get Resource Name', () => {
-    const name = common.getResourceName('KeyVault');
-    expect(name).to.be.equal('stack-keyvault');
+  it("Get Resource Name", () => {
+    const name = common.getResourceName("KeyVault");
+    expect(name).to.be.equal("stack-keyvault");
   });
 
-  it('Get Storage Name', () => {
-    const name = getStorageName('The-Main');
-    expect(name).to.be.equal('stackthemainstg');
+  it("Get Storage Name", () => {
+    const name = getStorageName("The-Main");
+    expect(name).to.be.equal("stackthemainstg");
   });
 
-  it('Get name space should be replace with hyphen', () => {
-    const name = common.getResourceName('This is my Name');
-    expect(name).to.be.equal('stack-this-is-my-name');
+  it("Get name space should be replace with hyphen", () => {
+    const name = common.getResourceName("This is my Name");
+    expect(name).to.be.equal("stack-this-is-my-name");
   });
 
-  it('Get secret name the stack name shall be removed', () => {
-    const name = getSecretName('test-stack-this is secret-name');
-    expect(name).to.be.equal('this-is-secret-name');
+  it("Get secret name the stack name shall be removed", () => {
+    const name = getSecretName("test-stack-this is secret-name");
+    expect(name).to.be.equal("test-this-is-secret-name");
   });
 
-  it('Get Resource Info from Id', () => {
+  it("Get Resource Info from Id", () => {
     const info = common.getResourceInfoFromId(
-      '/subscriptions/1234567890/resourceGroups/sg-dev-aks-vnet/providers/Microsoft.Network/virtualNetworks/sg-dev-vnet-trans'
+      "/subscriptions/1234567890/resourceGroups/sg-dev-aks-vnet/providers/Microsoft.Network/virtualNetworks/sg-dev-vnet-trans"
     );
 
-    expect(info!.name).to.be.equal('sg-dev-vnet-trans');
-    expect(info!.group.resourceGroupName).to.be.equal('sg-dev-aks-vnet');
-    expect(info!.subscriptionId).to.be.equal('1234567890');
+    expect(info!.name).to.be.equal("sg-dev-vnet-trans");
+    expect(info!.group.resourceGroupName).to.be.equal("sg-dev-aks-vnet");
+    expect(info!.subscriptionId).to.be.equal("1234567890");
   });
 
-  it('Prefix should not be duplicated', () => {
-    const name = common.getResourceName('stack-storage-name');
-    expect(name).to.be.equal('stack-storage-name');
+  it("Prefix should not be duplicated", () => {
+    const name = common.getResourceName("stack-storage-name");
+    expect(name).to.be.equal("stack-storage-name");
   });
 
-  it('Suffix should not be duplicated', () => {
-    const name = common.getResourceName('storage-name', {
-      suffix: 'hbd',
+  it("Suffix should not be duplicated", () => {
+    const name = common.getResourceName("storage-name", {
+      suffix: "hbd",
     });
-    expect(name).to.be.equal('stack-storage-name-hbd');
+    expect(name).to.be.equal("stack-storage-name-hbd");
   });
 });
--- z_tests/Vault/KeyVault.test.ts
+++ z_tests/Vault/KeyVault.test.ts
@@ -1,33 +1,34 @@
-import vaultCreator from '../../KeyVault';
-import '../_tools/Mocks';
-import { expect } from 'chai';
+import vaultCreator from "../../KeyVault";
+import "../_tools/Mocks";
+import { expect } from "chai";
 
-describe('Key Vault Creator tests', () => {
-  it('Vault Creator', async () => {
-    const group = { resourceGroupName: 'RG' };
+describe("Key Vault Creator tests", () => {
+  it("Vault Creator", async () => {
+    const group = { resourceGroupName: "RG" };
 
     const rs = await vaultCreator({
-      name: 'root',
+      name: "root",
       group,
     });
 
-    expect(rs.name).to.equal('test-stack-root-vlt');
+    expect(rs.name).to.equal("stack-root-vlt");
     expect(rs.toVaultInfo().group).to.equal(group);
 
-    rs.resource.urn.apply(n => expect(n).to.include('test-stack-root-vlt'));
+    rs.resource.urn.apply((n) => expect(n).to.include("stack-root-vlt"));
   }).timeout(5000);
 
-  it('Vault Creator with custom prefix', async () => {
-    const group = { resourceGroupName: 'RG' };
+  it("Vault Creator with custom prefix", async () => {
+    const group = { resourceGroupName: "RG" };
 
     const rs = await vaultCreator({
-      name: 'root',
+      name: "root",
+      nameConvention: { prefix: "steven", suffix: "vlt" },
       group,
     });
 
-    expect(rs.name).to.equal('test-stack-root-vlt');
+    expect(rs.name).to.equal("steven-root-vlt");
     expect(rs.toVaultInfo().group).to.equal(group);
 
-    rs.resource.urn.apply(n => expect(n).to.include('test-stack-root-vlt'));
+    rs.resource.urn.apply((n) => expect(n).to.include("steven-root-vlt"));
   }).timeout(5000);
 });
