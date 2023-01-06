263f56e0e0b4f67eb4ad9ba2f3a8b313e7456690
Tuan <singa@stadios.dev>
2023-01-06 14:55:10 +0700

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,20 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        parser: "@typescript-eslint/parser",
-        project: "./tsconfig.json",
-    },
-    plugins: ['@typescript-eslint'],
-    rules:{
-        "@typescript-eslint/no-floating-promises": "error"
-    },
-    root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "require-await": "off",
+    "@typescript-eslint/require-await": "error",
+  },
+  root: true,
 };
