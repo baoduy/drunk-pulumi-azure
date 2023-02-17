5af664fd1aaa3005d31210c102aa0a9762c72a32
Steven Hoang <drunkcoding@outlook.com>
2023-02-17 14:35:39 +0800

    Update AzureEnv.ts

--- Common/AzureEnv.ts
+++ Common/AzureEnv.ts
@@ -10,7 +10,7 @@ const config = output(native.authorization.getClientConfig());
 export const tenantId = config.tenantId;
 export const subscriptionId = config.subscriptionId;
 export const currentServicePrincipal = config.objectId;
-export const defaultLocation = "SoutheastAsia";
+export const defaultLocation = 'SoutheastAsia';
 export const defaultScope = subscriptionId.apply((s) => `/subscriptions/${s}`);
 
 //Print and Check
@@ -23,29 +23,32 @@ all([subscriptionId, tenantId]).apply(([s, t]) => {
 export const defaultTags = {
   environment: stack,
   organization: organization,
-  "pulumi-project": projectName,
+  'pulumi-project': projectName,
 };
 export enum Environments {
-  Global = "global",
-  Dev = "dev",
-  Sandbox = "sandbox",
-  Prd = "prd",
+  Global = 'global',
+  Local = 'local',
+  Dev = 'dev',
+  Sandbox = 'sandbox',
+  Prd = 'prd',
 }
 
 export const isEnv = (env: Environments) => stack.includes(env);
 export const isDev = isEnv(Environments.Dev);
 export const isSandbox = isEnv(Environments.Sandbox);
 export const isPrd = isEnv(Environments.Prd);
-/** Protect Private API that only allows accessing from APIM */
-//export const apimEnabled = isPrd;
+export const isGlobal = isEnv(Environments.Global);
+export const isLocal = isEnv(Environments.Local);
 
 const getCurrentEnv = () => {
+  if (isGlobal) return Environments.Global;
   if (isPrd) return Environments.Prd;
   if (isSandbox) return Environments.Sandbox;
-  if (isDev) return Environments.Dev;
 
+  if (isDev) return Environments.Dev;
   if (testMode) return Environments.Dev;
-  return Environments.Global;
+
+  return Environments.Local;
 };
 
 export const currentEnv = getCurrentEnv();
@@ -84,7 +87,7 @@ export const getResourceIdFromInfo = ({
   else if (name && provider)
     return interpolate`/subscriptions/${subscriptionId}/resourceGroups/${group.resourceGroupName}/providers/${provider}/${name}`;
 
-  throw new Error("Resource Info is invalid.");
+  throw new Error('Resource Info is invalid.');
 };
 
 /**Get Resource Info from Resource ID. Sample ID is "/subscriptions/01af663e-76dd-45ac-9e57-9c8e0d3ee350/resourceGroups/sandbox-codehbd-group-hbd/providers/Microsoft.Network/virtualNetworks/sandbox-codehbd-vnet-hbd"*/
@@ -98,14 +101,14 @@ export interface ResourceInfo {
 export const getResourceInfoFromId = (id: string): ResourceInfo | undefined => {
   if (!id) return undefined;
 
-  const details = id.split("/");
-  let name = "";
-  let groupName = "";
-  let subscriptionId = "";
+  const details = id.split('/');
+  let name = '';
+  let groupName = '';
+  let subscriptionId = '';
 
   details.forEach((d, index) => {
-    if (d === "subscriptions") subscriptionId = details[index + 1];
-    if (d === "resourceGroups" || d === "resourcegroups")
+    if (d === 'subscriptions') subscriptionId = details[index + 1];
+    if (d === 'resourceGroups' || d === 'resourcegroups')
       groupName = details[index + 1];
     if (index === details.length - 1) name = d;
   });
