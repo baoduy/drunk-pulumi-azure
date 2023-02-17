1df14eb3fa36855df3489f40815a4ab203326aa0
Steven Hoang <drunkcoding@outlook.com>
2023-02-17 14:45:25 +0800

    Update index.ts

--- KubeX/Tools/Dapr/index.ts
+++ KubeX/Tools/Dapr/index.ts
@@ -1,45 +1,39 @@
-import { DefaultK8sArgs, K8sArgs } from '../../types';
-import { KeyVaultInfo } from '../../../types';
-import { randomPassword } from '../../../Core/Random';
-import { StorageClassNameTypes } from '../../Storage';
+import { K8sArgs } from '../../types';
 import * as k8s from '@pulumi/kubernetes';
-import { addCustomSecret } from '../../../KeyVault/CustomHelper';
-import { getPasswordName } from '../../../Common/Naming';
-import { interpolate } from '@pulumi/pulumi';
 import Namespace from '../../Core/Namespace';
 import { DaprStorage } from './Storage';
 import Storage from './Storage';
+import { isLocal, isDev } from '../../../Common/AzureEnv';
 
 interface Props extends K8sArgs {
   name?: string;
-  namespace?: string;
+  //namespace?: string;
   version?: string;
   storage: DaprStorage;
 }
 
 export default async ({
   name = 'dapr',
-  namespace = 'dapr-system',
   version = '1.10',
   storage,
   provider,
   dependsOn,
 }: Props) => {
-  const ns = Namespace({ name: namespace, provider, dependsOn });
+  const ns = Namespace({ name: 'dapr-system', provider, dependsOn });
 
   const dapr = new k8s.helm.v3.Chart(
     name,
     {
       namespace: ns.metadata.name,
-      chart: 'dapr/dapr',
-      version,
+      chart: 'dapr',
+      //version,
       fetchOpts: { repo: 'https://dapr.github.io/helm-charts' },
       skipAwait: true,
 
       values: {
         global: {
-          tag: `${version}-mariner`,
-          ha: { enabled: true },
+          //tag: `${version}-mariner`,
+          ha: { enabled: !isLocal && !isDev },
         },
       },
     },
