086c54306c8b210e3eb3b0218712a99e50ca2cf3
Steven Hoang <hoangbaoduy@gmail.com>
2023-03-09 10:14:53 +0800

    add app services

---
+++ Web/WebApp.ts
@@ -0,0 +1,40 @@
+import * as native from '@pulumi/azure-native';
+import { BasicResourceArgs } from '../types';
+import { getWebAppName } from '../Common/Naming';
+import Locker from '../Core/Locker';
+import * as pulumi from '@pulumi/pulumi';
+import { SiteConfigArgs } from './types';
+
+interface Props extends BasicResourceArgs {
+  kind: 'FunctionApp' | 'WebApp';
+  appServicePlanId: pulumi.Output<string>;
+  siteConfig?: SiteConfigArgs;
+  enabled?: boolean;
+  lock?: boolean;
+}
+
+export default ({
+  name,
+  group,
+  kind,
+  appServicePlanId,
+  siteConfig,
+  enabled,
+  lock,
+}: Props) => {
+  const finalName = getWebAppName(name);
+
+  const app = new native.web.WebApp(finalName, {
+    kind,
+    ...group,
+    serverFarmId: appServicePlanId,
+    enabled,
+    httpsOnly: true,
+    siteConfig,
+  });
+
+  if (lock) {
+    Locker({ name, resourceId: app.id, dependsOn: app });
+  }
+  return app;
+};
