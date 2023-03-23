f3f7a223974489199b3c2d0709a7c2c537e281e2
Steven Hoang <hoangbaoduy@gmail.com>
2023-03-23 16:27:18 +0800

    update packages

--- Tools/Axios.ts
+++ Tools/Axios.ts
@@ -1,6 +1,6 @@
-import { InternalCredentials } from "../CustomProviders/Base/Credentials";
-import axios, { AxiosRequestHeaders } from "axios";
-import { urlJoin } from "url-join-ts";
+import { InternalCredentials } from '../CustomProviders/Base/Credentials';
+import axios, { AxiosRequestHeaders } from 'axios';
+import { urlJoin } from 'url-join-ts';
 
 export const createAxios = () => {
   const credentials = new InternalCredentials();
@@ -8,25 +8,25 @@ export const createAxios = () => {
   let baseUrl: string | undefined;
 
   const axiosWrapper = axios.create({
-    baseURL: "",
+    baseURL: '',
   });
 
   axiosWrapper.interceptors.request.use(async (config) => {
     if (!token) {
       const tokenRequest = await credentials.getCredentials();
       token = (await tokenRequest.getToken()).accessToken;
-      baseUrl = `https://management.azure.com/subscriptions/${credentials.subscriptionID}`;
+      baseUrl = `https://management.azure.com/subscriptions/${credentials.subscriptionID!}`;
     }
 
-    if (!config.url || !config.url.startsWith("http")) {
-      config.url = config.url!.includes("subscriptions")
-        ? urlJoin("https://management.azure.com", config.url!)
-        : urlJoin(baseUrl, config.url!);
+    if (!config.url || !config.url.startsWith('http')) {
+      config.url = config.url.includes('subscriptions')
+        ? urlJoin('https://management.azure.com', config.url)
+        : urlJoin(baseUrl, config.url);
     }
-      (config.headers as AxiosRequestHeaders).set(
-        "Authorization",
-        token ? `Bearer ${token}` : ""
-      );
+
+    if (token) {
+      config.headers.set('Authorization', token);
+    }
 
     return config;
   });
