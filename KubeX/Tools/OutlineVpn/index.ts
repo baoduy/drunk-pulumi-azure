190d4e5870a6aae0a5d8d177ab1e5907ece93e17
Steven Hoang <hoangbaoduy@gmail.com>
2023-06-09 17:20:04 +0800

    update

--- KubeX/Tools/OutlineVpn/index.ts
+++ KubeX/Tools/OutlineVpn/index.ts
@@ -2,12 +2,14 @@ import { K8sArgs } from '../../types';
 import Namespace from '../../Core/Namespace';
 import { KeyVaultInfo } from '../../../types';
 import { certImportFromVault, certImportFromFolder } from '../../CertImports';
+import * as kubernetes from "@pulumi/kubernetes";
 
 interface Props extends K8sArgs {
   vaultInfo?: KeyVaultInfo;
   certVaultName?: string;
   certFolderName?: string;
 }
+
 
 export default async ({
   vaultInfo,
@@ -19,22 +21,106 @@ export default async ({
   const namespace = 'outline-system';
 
   const ns = Namespace({ name: namespace, ...others });
+const defaultProps = {
+  namespace,
+  dependsOn: ns,
+  provider: others.provider,}
 
   if (certVaultName && vaultInfo) {
     await certImportFromVault({
       certNames: [certVaultName],
       vaultInfo,
-      namespace,
-      dependsOn: ns,
-      provider: others.provider,
+      ...defaultProps
     });
   } else if (certFolderName) {
     await certImportFromFolder({
       certName: name,
       certFolder: certFolderName,
       namespaces: [namespace],
-      dependsOn: ns,
-      provider: others.provider,
+      ...defaultProps
     });
   }
+
+  //Deploy nfs-server
+  const nfs_serverDeployment = new kubernetes.extensions.v1beta1.Deployment("nfs_serverDeployment", {
+    metadata: {
+      name: "nfs-server",
+    },
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: {
+          role: "nfs-server",
+        },
+      },
+      template: {
+        metadata: {
+          labels: {
+            role: "nfs-server",
+          },
+        },
+        spec: {
+          containers: [{
+            name: "nfs-server",
+            image: "gcr.io/google_containers/volume-nfs:0.8",
+            ports: [
+              {
+                name: "nfs",
+                containerPort: 2049,
+              },
+              {
+                name: "mountd",
+                containerPort: 20048,
+              },
+              {
+                name: "rpcbind",
+                containerPort: 111,
+              },
+            ],
+            securityContext: {
+              privileged: true,
+            },
+            volumeMounts: [{
+              mountPath: "/exports",
+              name: "mypvc",
+            }],
+          }],
+          volumes: [{
+            name: "mypvc",
+            gcePersistentDisk: {
+              pdName: "gce-nfs-disk",
+              fsType: "ext4",
+            },
+          }],
+        },
+      },
+    },
+  });
+
+  const nfs_serverService = new kubernetes.core.v1.Service("nfs_serverService", {
+    metadata: {
+      name: "nfs-server",
+    },
+    spec: {
+      ports: [
+        {
+          name: "nfs",
+          port: 2049,
+        },
+        {
+          name: "mountd",
+          port: 20048,
+        },
+        {
+          name: "rpcbind",
+          port: 111,
+        },
+      ],
+      selector: {
+        role: "nfs-server",
+      },
+    },
+  });
+
+
 };
