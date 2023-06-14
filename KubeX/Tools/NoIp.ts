2d09fbd9291c0c88eeeb18d64a12c124ce8f44a1
Steven Hoang <hoangbaoduy@gmail.com>
2023-06-14 19:36:50 +0800

    Create NoIp.ts

---
+++ KubeX/Tools/NoIp.ts
@@ -0,0 +1,44 @@
+import * as k8s from '@pulumi/kubernetes';
+import { Input, Resource } from '@pulumi/pulumi';
+
+import deployment, { IngressTypes } from '../Deployment';
+
+export interface HelloAppProps {
+  namespace: Input<string>;
+  username: Input<string>;
+  password: Input<string>;
+  domain: Input<string>;
+  interval?:number;
+  provider: k8s.Provider;
+  dependsOn?: Input<Input<Resource>[]> | Input<Resource>;
+}
+
+export default async ({
+  namespace,
+  username,
+  password, domain,
+  interval = 5,
+  ...others
+}: HelloAppProps) => {
+  const name = 'no-ip';
+  const image = 'aanousakis/no-ip:v1';
+
+  await deployment({
+    name,
+    namespace,
+
+    configMap:{
+      INTERVAL:interval.toString(),
+      DOMAINS:domain
+    },
+    secrets:{USERNAME:username,PASSWORD:password},
+
+    podConfig: {
+      image,
+      resources: { requests: { memory: '1Mi', cpu: '1m' } },
+    },
+    deploymentConfig: { replicas: 1 },
+
+    ...others,
+  });
+};
