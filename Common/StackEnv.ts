c5b81411802650ad17a8243f63a78bbbe1c246ec
Steven Hoang <drunkcoding@outlook.com>
2024-03-06 14:05:40 +0800

    Update StackEnv.ts

--- Common/StackEnv.ts
+++ Common/StackEnv.ts
@@ -1,16 +1,23 @@
 import * as pulumi from '@pulumi/pulumi';
 
-console.log(
-  'Pulumi Environments\n',
-  Object.keys(process.env)
-    .filter((key) => key.startsWith('PULUMI_'))
-    .map((key) => `\t${key}: ${process.env[key]}`)
-    .join('\n')
-);
-
 export const isDryRun = Boolean(process.env.PULUMI_NODEJS_DRY_RUN);
 export const organization = process.env.PULUMI_NODEJS_ORGANIZATION!;
 export const projectName =
   process.env.PULUMI_NODEJS_PROJECT ?? pulumi.getProject().toLowerCase();
 export const stack =
   process.env.PULUMI_NODEJS_STACK ?? pulumi.getStack().toLowerCase();
+
+// console.log(
+//     'Pulumi Environments\n',
+//     Object.keys(process.env)
+//         .filter((key) => key.startsWith('PULUMI_'))
+//         .map((key) => `\t${key}: ${process.env[key]}`)
+//         .join('\n')
+// );
+
+console.log('Pulumi Environments:', {
+  organization,
+  projectName,
+  stack,
+  isDryRun,
+});
