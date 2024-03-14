f0d0c9af4b5bb0b1ec245a142042f9b5b319247f
Steven Hoang <hoangbaoduy@gmail.com>
2024-03-14 15:42:06 +0800

    update key vault always enable RBAC

--- KeyVault/VaultAccess.ts
+++ KeyVault/VaultAccess.ts
@@ -8,7 +8,7 @@ import * as azuread from '@pulumi/azuread';
 export type VaultAccessType = {
   /** Grant permission of this group into Environment Roles groups*/
   envRoleNames?: EnvRoleNamesType;
-  permissions?: Array<PermissionProps>;
+  //permissions?: Array<PermissionProps>;
   includeOrganization?: boolean;
 };
 
@@ -38,27 +38,27 @@ export default async ({ name, auth }: Props) => {
       });
 
   //Add current service principal in
-  if (auth.permissions == undefined) {
-    auth.permissions = [
-      // {
-      //   objectId: currentServicePrincipal,
-      //   permission: 'ReadWrite',
-      // },
-    ];
-  }
+  // if (auth.permissions == undefined) {
+  //   auth.permissions = [
+  //     // {
+  //     //   objectId: currentServicePrincipal,
+  //     //   permission: 'ReadWrite',
+  //     // },
+  //   ];
+  // }
 
   //Add Permission to Groups
-  auth.permissions.forEach(
-    ({ objectId, applicationId, permission, ...others }, index) =>
-      new azuread.GroupMember(`${name}-${permission}-${index}`, {
-        groupObjectId:
-          permission === 'ReadOnly'
-            ? readOnlyGroup.objectId
-            : adminGroup.objectId,
-        memberObjectId: objectId ?? applicationId,
-        ...others,
-      })
-  );
+  // auth.permissions.forEach(
+  //   ({ objectId, applicationId, permission, ...others }, index) =>
+  //     new azuread.GroupMember(`${name}-${permission}-${index}`, {
+  //       groupObjectId:
+  //         permission === 'ReadOnly'
+  //           ? readOnlyGroup.objectId
+  //           : adminGroup.objectId,
+  //       memberObjectId: objectId ?? applicationId,
+  //       ...others,
+  //     })
+  // );
 
   return { readOnlyGroup, adminGroup };
 };
