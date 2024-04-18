3487157dc769bd8c6ce45b383163cbe160f51801
Steven Hoang <drunkcoding@outlook.com>
2024-04-18 15:29:03 +0800

    Update AutoTags.ts

--- src/Common/AutoTags.ts
+++ src/Common/AutoTags.ts
@@ -1,14 +1,14 @@
-import { runtime } from "@pulumi/pulumi";
+import { runtime } from '@pulumi/pulumi';
 
 const ignoredTags = [
-  "group:Group",
-  "groupMember:GroupMember",
-  "application:Application",
-  "applicationPassword:ApplicationPassword",
-  "servicePrincipal:ServicePrincipal",
-  "servicePrincipalPassword:ServicePrincipalPassword",
-  "kubernetes",
-  "cloudflare",
+  'Group',
+  'GroupMember',
+  'Application',
+  'ApplicationPassword',
+  'ServicePrincipal',
+  'ServicePrincipalPassword',
+  'kubernetes',
+  'cloudflare',
 ];
 
 export function registerAutoTags(autoTags: Record<string, string>): void {
@@ -18,7 +18,7 @@ export function registerAutoTags(autoTags: Record<string, string>): void {
       return { props: args.props, opts: args.opts };
 
     //Apply default tag
-    args.props["tags"] = { ...args.props["tags"], ...autoTags };
+    args.props['tags'] = { ...args.props['tags'], ...autoTags };
     return { props: args.props, opts: args.opts };
   });
 }
