9f2dffab9687b76bc2b6fd37af5ef5334b1073f1
Steven Hoang <drunkcoding@outlook.com>
2024-05-29 15:04:17 +0800

    Update Extension.ts

--- src/VM/Extension.ts
+++ src/VM/Extension.ts
@@ -31,7 +31,7 @@ export default ({
       ...extension,
       autoUpgradeMinorVersion: true,
       enableAutomaticUpgrade,
-      suppressFailures: true,
+      suppressFailures: false,
     },
     { dependsOn },
   );
