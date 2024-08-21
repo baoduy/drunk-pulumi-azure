39cfc5432ab55d860b9e1df0aa70672827a143bf
Steven Hoang <hoangbaoduy@gmail.com>
2024-08-21 18:56:28 +0800

    fix re-export issues

--- src/AzAd/EnvRoles/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './EnvRoles.Consts';
-export * from './EnvRoles';
--- src/AzAd/Identities/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AzDevOpsIdentity } from './AzDevOpsIdentity';
-export { default as AzDevOpsManagedIdentity } from './AzDevOpsManagedIdentity';
-export { default as AzUserAdRevertSync } from './AzUserAdRevertSync';
-export * as EnvUID from './EnvUID';
